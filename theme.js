7f4bd46fbbc10976767afce73c6d670b4d44d0c1
Just EnginErin <31420768+just-enginerin@users.noreply.github.com>
2023-01-15 21:53:08 -0500

    Assignment 2.1 - Palindrome App
    - Move all theme-related JavaScript code into the theme.js file, to be imported into each page.
    - Create a Palindrome App

--- theme.js
+++ theme.js
@@ -7,7 +7,7 @@ Description: Functionality to update the site's theme styles.
 */
 
 /**
- * Set the default theme to light if one has not been set in the browsers localStorage
+ * Function to set the default theme to light if one has not been set in the browsers localStorage
 */
 const setDefaultTheme = () => {
   const theme = localStorage.getItem("mode") || "light-theme";
@@ -24,4 +24,34 @@ const setDefaultTheme = () => {
 */
 const setSelectedTheme = () => {
   document.body.classList.value = localStorage.getItem("mode") || "light-theme";
+}
+
+/**
+ * Function to switch the users selected website theme
+ * @param x - this instance
+*/
+const toggleMode = (x) => {
+  let colorTheme = document.body.classList; // get the body's CSS class
+  let iconMode = x.classList; // get the current classes assigned to the triggered button
+
+  // If the current body class is set to the light-theme, update the user's preference to the dark-theme in the browsers local storage.
+  if (colorTheme.value === "light-theme") {
+    localStorage.clear();
+    localStorage.setItem("mode", "dark-theme");
+    localStorage.setItem("iconMode", "fa-toggle-on");
+    localStorage.setItem("iconText", "Dark Mode");
+  }
+  
+  // If the current body class is set to the dark-theme, update the user's preference to the light-theme in the browsers local storage.
+  else {
+    localStorage.clear();
+    localStorage.setItem("mode", "light-theme");
+    localStorage.setItem("iconMode", "fa-toggle-off");
+    localStorage.setItem("iconText", "Light Mode");
+  }
+
+  // Apply the updated selection to the HTML page elements
+  colorTheme.value = localStorage.getItem("mode");
+  iconMode.value = `fa ${localStorage.getItem("iconMode")} pull-right`;
+  document.getElementById("icon-text").innerHTML = localStorage.getItem("iconText");
 }
